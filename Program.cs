ac2a817097cf4acf15ab3da4b3555410c889e484
Kawan Weber <kawan_weber@hotmail.com>
2025-03-10 17:37:19 -0300

    TIMES

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿//BRASIL
+Console.BackgroundColor = ConsoleColor.Green;
+Console.ForegroundColor = ConsoleColor.White;
+Console.Write("B");
+
+Console.ForegroundColor = ConsoleColor.White;
+Console.Write("r");
+
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Write("as");
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.Write("il");
+
+Console.ResetColor();
+Console.WriteLine();
+
+
+//Alemanha
+Console.ResetColor();
+Console.WriteLine();
+
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.Write("G");
+
+Console.ForegroundColor = ConsoleColor.Black;
+Console.Write("e");
+
+Console.BackgroundColor = ConsoleColor.Red;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.Write("r");
+
+Console.ForegroundColor = ConsoleColor.Black;
+Console.Write("m");
+
+Console.ForegroundColor = ConsoleColor.Black;
+Console.Write("a");
+
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.Write("n");
+
+Console.ForegroundColor = ConsoleColor.Black;
+Console.Write("y");
+
+Console.ResetColor();
+Console.WriteLine();
+
+
+//PORTUGAL
+Console.ResetColor();
+Console.WriteLine();
+
+Console.BackgroundColor = ConsoleColor.DarkGreen;
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Write("Por");
+
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Write("tu");
+
+Console.BackgroundColor = ConsoleColor.DarkRed;
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Write("gal");
+
+Console.ResetColor();
+Console.WriteLine();
